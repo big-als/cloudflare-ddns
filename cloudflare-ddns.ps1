5078db7fb935ffc90f286f50b992b94d43961e69
Debian <alexh@Docker01.horst.home>
2024-12-15 13:40:14 -0500

    Adjust timestamp formatting.

--- cloudflare-ddns.ps1
+++ cloudflare-ddns.ps1
@@ -12,6 +12,8 @@ param (
     $Record
 )
 
+$date = Get-Date -Format "dd/MM/yyyy HH:mm UTCK"
+
 # Build the request headers once. These headers will be used throughout the script.
 $headers = @{
     "X-Auth-Email"  = $($Email)
@@ -27,11 +29,11 @@ $uri = "https://api.cloudflare.com/client/v4/user/tokens/verify"
 
 $auth_result = Invoke-RestMethod -Method GET -Uri $uri -Headers $headers -SkipHttpErrorCheck
 if (-not($auth_result.result)) {
-    Write-Output "API token validation failed. Error: $($auth_result.errors.message). Terminating script."
+    Write-Output "[$($date)] API token validation failed. Error: $($auth_result.errors.message). Terminating script."
     # Exit script
     return
 }
-Write-Output "API token validation [$($Token)] success. $($auth_result.messages.message)."
+Write-Output "[$($date)] API token validation [$($Token)] success. $($auth_result.messages.message)."
 #EndRegion
 
 #Region Get Zone ID
@@ -39,13 +41,13 @@ Write-Output "API token validation [$($Token)] success. $($auth_result.messages.
 $uri = "https://api.cloudflare.com/client/v4/zones?name=$($Domain)"
 $DnsZone = Invoke-RestMethod -Method GET -Uri $uri -Headers $headers -SkipHttpErrorCheck
 if (-not($DnsZone.result)) {
-    Write-Output "Search for the DNS domain [$($Domain)] return zero results. Terminating script."
+    Write-Output "[$($date)] Search for the DNS domain [$($Domain)] return zero results. Terminating script."
     # Exit script
     return
 }
 ## Store the DNS zone ID
 $zone_id = $DnsZone.result.id
-Write-Output "Domain zone [$($Domain)]: ID=$($zone_id)"
+Write-Output "[$($date)] Domain zone [$($Domain)]: ID=$($zone_id)"
 #End Region
 
 #Region Get DNS Record
@@ -53,7 +55,7 @@ Write-Output "Domain zone [$($Domain)]: ID=$($zone_id)"
 $uri = "https://api.cloudflare.com/client/v4/zones/$($zone_id)/dns_records?name=$($Record)"
 $DnsRecord = Invoke-RestMethod -Method GET -Uri $uri -Headers $headers -SkipHttpErrorCheck
 if (-not($DnsRecord.result)) {
-    Write-Output "Search for the DNS record [$($Record)] return zero results. Terminating script."
+    Write-Output "[$($date)] Search for the DNS record [$($Record)] return zero results. Terminating script."
     # Exit script
     return
 }
@@ -67,19 +69,19 @@ $record_id = $DnsRecord.result.id
 $record_ttl = $DnsRecord.result.ttl
 ## Store the DNS record proxied value
 $record_proxied = $DnsRecord.result.proxied
-Write-Output "DNS record [$($Record)]: Type=$($record_type), IP=$($old_ip)"
+Write-Output "[$($date)] DNS record [$($Record)]: Type=$($record_type), IP=$($old_ip)"
 #EndRegion
 
 #Region Get Current Public IP Address
 $new_ip = Invoke-RestMethod -Uri 'https://v4.ident.me'
-Write-Output "Public IP Address: OLD=$($old_ip), NEW=$($new_ip)"
+Write-Output "[$($date)] Public IP Address: OLD=$($old_ip), NEW=$($new_ip)"
 #EndRegion
 
 #Region update Dynamic DNS Record
 ## Compare current IP address with the DNS record
 ## If the current IP address does not match the DNS record IP address, update the DNS record.
 if ($new_ip -ne $old_ip) {
-    Write-Output "The current IP address does not match the DNS record IP address. Attempt to update."
+    Write-Output "[$($date)] The current IP address does not match the DNS record IP address. Attempt to update."
     ## Update the DNS record with the new IP address
     $uri = "https://api.cloudflare.com/client/v4/zones/$($zone_id)/dns_records/$($record_id)"
     $body = @{
@@ -92,15 +94,15 @@ if ($new_ip -ne $old_ip) {
 
     $Update = Invoke-RestMethod -Method PUT -Uri $uri -Headers $headers -SkipHttpErrorCheck -Body $body
     if (($Update.errors)) {
-        Write-Output "DNS record update failed. Error: $($Update[0].errors.message)"
+        Write-Output "[$($date)] DNS record update failed. Error: $($Update[0].errors.message)"
         ## Exit script
         return
     }
 
-    Write-Output "DNS record update successful."
+    Write-Output "[$($date)] DNS record update successful."
     return ($Update.result)
 }
 else {
-    Write-Output "The current IP address and DNS record IP address are the same. There's no need to update."
+    Write-Output "[$($date)] The current IP address and DNS record IP address are the same. There's no need to update."
 }
 #EndRegion
